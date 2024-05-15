c6aee6dfb03bd7d581efe91a41c9f8293a5bdc6f
gwennbrln <bgwenn33@gmail.com>
2024-05-15 09:29:15 +0200

    changement nom classe

---
+++ src/projet/MauvaiseCoordonneException.java
@@ -0,0 +1,7 @@
+package src.projet;
+public class MauvaiseCoordonneException extends Exception {
+    // Constructeur qui prend un message d'erreur
+    public MauvaiseCoordonneException(String message) {
+        super(message);
+    }
+}
