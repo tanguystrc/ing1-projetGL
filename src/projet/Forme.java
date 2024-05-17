bf6b6272f1465b9ae1246e50e4b48119ae0d0eb2
gwennbrln <bgwenn33@gmail.com>
2024-05-17 11:32:41 +0200

    arborescence

--- src/projet/Forme.java
+++ src/projet/Forme.java
@@ -1,9 +1,16 @@
 package src.projet;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+
 import java.awt.Color;
 
 /**
@@ -15,12 +22,17 @@ public abstract class Forme {
     protected BufferedImage image1;
     protected BufferedImage image2;
     protected int nbFrame;
+    private javafx.scene.paint.Color selectedColor;
 
     public Forme(PointDeControle pointsDeControle, BufferedImage image1, BufferedImage image2, int nbFrame) {
         this.pointsDeControle = pointsDeControle;
         this.image1 = image1;
         this.image2 = image2;
         this.nbFrame = nbFrame;
+    }
+
+    public void setSelectedColor(javafx.scene.paint.Color selectedColor) {
+        this.selectedColor = selectedColor;
     }
 
     /**
@@ -94,6 +106,53 @@ public abstract class Forme {
         }
         return p;
     }
+     /**
+     * Applique le morphisme simple pour générer une animation GIF.
+     * @param image1 l'image source
+     * @param pointsDeControle les points de contrôle
+     * @param nbFrame le nombre de frames
+     * @throws IOException si une erreur d'entrée/sortie se produit
+     */
+    public void morphisme(BufferedImage image1, PointDeControle pointsDeControle, int nbFrame) throws IOException {
+        Color[][] matrix = genererMatrice(image1);
+        List<Point> listIndice = listIndice(pointsDeControle, nbFrame); 
+        List<Point> pointsKeys = new ArrayList<>(pointsDeControle.getPointsMap().keySet());
+        Color couleur = (selectedColor != null) ? new Color((int)(selectedColor.getRed() * 255), (int)(selectedColor.getGreen() * 255), (int)(selectedColor.getBlue() * 255)) : chercheCouleur(matrix, pointsKeys);
+        Color autreCouleur = chercheAutreCouleur(matrix, pointsKeys);
+    
+        ImageOutputStream output = new FileImageOutputStream(new File("animation.gif"));
+        GifSequenceWriter gifWriter = new GifSequenceWriter(output, image1.getType(), 100, true);
+    
+        int hauteur = matrix.length;
+        int largeur = matrix[0].length;
+    
+        for (int i = 0; i < nbFrame; i++) {
+            List<Point> listPoint = new ArrayList<>(); 
+            for (int j = 0; j < pointsKeys.size(); j++) {
+                Point p = pointsKeys.get(j);
+                Point indice = listIndice.get(j);
+                double x = Math.max(0, Math.min(largeur - 1, p.getX() + indice.getX() * i));
+                double y = Math.max(0, Math.min(hauteur - 1, p.getY() + indice.getY() * i));
+                Point p1 = new Point(x, y); 
+                listPoint.add(p1);
+            }
+            BufferedImage frameImage = morphismeRemplissage(matrix, couleur, autreCouleur, listPoint);
+            
+            gifWriter.writeToSequence(frameImage);
+        }
+    
+        // Ajouter l'image d'arrivée en utilisant les valeurs de la map en entrée
+        List<Point> listPointArrivee = new ArrayList<>();
+        for (Entry<Point, Point> entry : pointsDeControle.getPointsMap().entrySet()) {
+            listPointArrivee.add(entry.getValue());
+        }
+        BufferedImage imageArrivee = morphismeRemplissage(matrix, couleur, autreCouleur, listPointArrivee);
+        gifWriter.writeToSequence(imageArrivee);
+    
+        gifWriter.close();
+        output.close();
+    }
+    
 
     public abstract boolean estDomaine(List<Point> listePoint, Point p) ;
         /**
--- src/projet/FormeLineaire.java
+++ src/projet/FormeLineaire.java
@@ -1,29 +1,18 @@
 package src.projet;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
+
 import java.util.List;
 import java.awt.Color;
-import java.util.Map.Entry;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FormeLineaire extends Forme {
     private Color[][] matrix1;
     private Color[][] matrix2;
-    private javafx.scene.paint.Color selectedColor;
 
     public FormeLineaire(PointDeControle pointsDeControle, int nbFrame, Color[][] matrix1, Color[][] matrix2) {
         super(pointsDeControle, null, null, nbFrame);
         this.matrix1 = matrix1;
         this.matrix2 = matrix2;
-    }
-
-    public void setSelectedColor(javafx.scene.paint.Color selectedColor) {
-        this.selectedColor = selectedColor;
     }
 
     @Override
@@ -81,53 +70,7 @@ public class FormeLineaire extends Forme {
         }
         return compteur % 2 != 0;
     }
-    /**
-     * Applique le morphisme simple pour générer une animation GIF.
-     * @param image1 l'image source
-     * @param pointsDeControle les points de contrôle
-     * @param nbFrame le nombre de frames
-     * @throws IOException si une erreur d'entrée/sortie se produit
-     */
-    public void morphismeSimple(BufferedImage image1, PointDeControle pointsDeControle, int nbFrame) throws IOException {
-        Color[][] matrix = genererMatrice(image1);
-        List<Point> listIndice = listIndice(pointsDeControle, nbFrame); 
-        List<Point> pointsKeys = new ArrayList<>(pointsDeControle.getPointsMap().keySet());
-        Color couleur = (selectedColor != null) ? new Color((int)(selectedColor.getRed() * 255), (int)(selectedColor.getGreen() * 255), (int)(selectedColor.getBlue() * 255)) : chercheCouleur(matrix, pointsKeys);
-        Color autreCouleur = chercheAutreCouleur(matrix, pointsKeys);
-    
-        ImageOutputStream output = new FileImageOutputStream(new File("animation.gif"));
-        GifSequenceWriter gifWriter = new GifSequenceWriter(output, image1.getType(), 100, true);
-    
-        int hauteur = matrix.length;
-        int largeur = matrix[0].length;
-    
-        for (int i = 0; i < nbFrame; i++) {
-            List<Point> listPoint = new ArrayList<>(); 
-            for (int j = 0; j < pointsKeys.size(); j++) {
-                Point p = pointsKeys.get(j);
-                Point indice = listIndice.get(j);
-                double x = Math.max(0, Math.min(largeur - 1, p.getX() + indice.getX() * i));
-                double y = Math.max(0, Math.min(hauteur - 1, p.getY() + indice.getY() * i));
-                Point p1 = new Point(x, y); 
-                listPoint.add(p1);
-            }
-            BufferedImage frameImage = morphismeRemplissage(matrix, couleur, autreCouleur, listPoint);
-            
-            gifWriter.writeToSequence(frameImage);
-        }
-    
-        // Ajouter l'image d'arrivée en utilisant les valeurs de la map en entrée
-        List<Point> listPointArrivee = new ArrayList<>();
-        for (Entry<Point, Point> entry : pointsDeControle.getPointsMap().entrySet()) {
-            listPointArrivee.add(entry.getValue());
-        }
-        BufferedImage imageArrivee = morphismeRemplissage(matrix, couleur, autreCouleur, listPointArrivee);
-        gifWriter.writeToSequence(imageArrivee);
-    
-        gifWriter.close();
-        output.close();
-    }
-    
+   
     
 
     
