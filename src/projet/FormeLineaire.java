78df5bbb28990a46a5e76bef332cf76b0b04dfe4
tangoat <tanguy.soutric@eisti.fr>
2024-05-21 14:46:51 +0200

    part 2 fixed

--- src/projet/FormeLineaire.java
+++ src/projet/FormeLineaire.java
@@ -41,36 +41,4 @@ public class FormeLineaire extends Forme {
         FormeLineaire other = (FormeLineaire) obj;
         return Arrays.deepEquals(matrix1, other.matrix1) && Arrays.deepEquals(matrix2, other.matrix2);
     }
-
-
-    public boolean estDomaine(List<Point> listePoint, Point p) {
-        int compteur = 0;
-        int nbPts = listePoint.size();
-        Point dernierPoint = listePoint.get(nbPts - 1);
-    
-        for (Point pointActuel : listePoint) {
-            if (pointActuel.getY() == p.getY() && dernierPoint.getY() == p.getY()) {
-                if ((pointActuel.getX() <= p.getX() && p.getX() <= dernierPoint.getX()) || 
-                    (dernierPoint.getX() <= p.getX() && p.getX() <= pointActuel.getX())) {
-                    return true; 
-                }
-            } else if (pointActuel.getX() == p.getX() && dernierPoint.getX() == p.getX()) {
-                if ((pointActuel.getY() <= p.getY() && p.getY() <= dernierPoint.getY()) || 
-                    (dernierPoint.getY() <= p.getY() && p.getY() <= pointActuel.getY())) {
-                    return true; 
-                }
-            } else {
-                if ((pointActuel.getY() < p.getY() && dernierPoint.getY() >= p.getY()) || 
-                    (pointActuel.getY() >= p.getY() && dernierPoint.getY() < p.getY())) {
-                    double intersectX = pointActuel.getX() + (p.getY() - pointActuel.getY()) * (dernierPoint.getX() - pointActuel.getX()) / (dernierPoint.getY() - pointActuel.getY());
-                    if (p.getX() < intersectX) {
-                        compteur++;
-                    }
-                }
-            }
-            dernierPoint = pointActuel;
-        }
-        return compteur % 2 != 0;
-    }
-    
 }
