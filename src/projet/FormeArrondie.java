835f5fba1f7a1819e905e5bf6ad5826bae5c067f
nolhan <aurenolhan@cy-tech.fr>
2024-05-21 11:23:51 +0200

    cool

---
+++ src/projet/FormeArrondie.java
@@ -0,0 +1,86 @@
+package src.projet;
+
+import java.awt.Color;
+import java.util.List;
+
+public class FormeArrondie extends Forme{
+    private Color[][] matrice1;
+    private Color[][] matrice2;
+
+    public FormeArrondie(PointDeControle pointsDeControle, int nbFrame, Color[][] matrice1, Color[][] matrice2) {
+        super(pointsDeControle, null, null, nbFrame);
+        this.matrice1 = matrice1;
+        this.matrice2 = matrice2;
+    }
+
+    public Color[][] getMatrice1() {
+        return matrice1;
+    }
+
+    public void setMatrice1(Color[][] matrice1) {
+        this.matrice1 = matrice1;
+    }
+
+    public Color[][] getMatrice2() {
+        return matrice2;
+    }
+
+    public void setMatrice2(Color[][] matrice2) {
+        this.matrice2 = matrice2;
+    }
+
+    public boolean estDomaine(List<Point> listePoints, Point point) {
+        int traversées = 0;
+
+        for (int i = 0; i < listePoints.size() - 3; i += 3) {
+            Point p0 = listePoints.get(i);
+            Point p1 = listePoints.get(i + 1);
+            Point p2 = listePoints.get(i + 2);
+            Point p3 = listePoints.get(i + 3);
+
+            if (traverseCourbe(p0, p1, p2, p3, point)) {
+                traversées++;
+            }
+        }
+
+        return (traversées % 2 != 0);
+    }
+
+    private boolean traverseCourbe(Point p0, Point p1, Point p2, Point p3, Point point) {
+        final int étapes = 100; // Nombre de points d'évaluation sur la courbe
+        double[] xPoints = new double[étapes];
+        double[] yPoints = new double[étapes];
+
+        for (int i = 0; i < étapes; i++) {
+            double t = (double) i / (étapes - 1);
+            double x = Math.pow(1 - t, 3) * p0.getX() +
+                       3 * t * Math.pow(1 - t, 2) * p1.getX() +
+                       3 * Math.pow(t, 2) * (1 - t) * p2.getX() +
+                       Math.pow(t, 3) * p3.getX();
+            double y = Math.pow(1 - t, 3) * p0.getY() +
+                       3 * t * Math.pow(1 - t, 2) * p1.getY() +
+                       3 * Math.pow(t, 2) * (1 - t) * p2.getY() +
+                       Math.pow(t, 3) * p3.getY();
+
+            xPoints[i] = x;
+            yPoints[i] = y;
+        }
+
+        for (int i = 0; i < étapes - 1; i++) {
+            if (segmentTraverseLigne(xPoints[i], yPoints[i], xPoints[i + 1], yPoints[i + 1], point.getX(), point.getY())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean segmentTraverseLigne(double x1, double y1, double x2, double y2, double px, double py) {
+        if (((y1 <= py && py < y2) || (y2 <= py && py < y1)) &&
+            px < ((x2 - x1) * (py - y1) / (y2 - y1) + x1)) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- src/projet/FormeLineaire.java
+++ src/projet/FormeLineaire.java
@@ -1,30 +1,18 @@
 package src.projet;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
+
 import java.util.List;
 import java.awt.Color;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FormeLineaire extends Forme {
     private Color[][] matrix1;
     private Color[][] matrix2;
-    private javafx.scene.paint.Color selectedColor;
 
     public FormeLineaire(PointDeControle pointsDeControle, int nbFrame, Color[][] matrix1, Color[][] matrix2) {
         super(pointsDeControle, null, null, nbFrame);
         this.matrix1 = matrix1;
         this.matrix2 = matrix2;
-    }
-
-    public void setSelectedColor(javafx.scene.paint.Color selectedColor) {
-        this.selectedColor = selectedColor;
     }
 
     @Override
@@ -42,106 +30,48 @@ public class FormeLineaire extends Forme {
         return Arrays.deepEquals(matrix1, other.matrix1) && Arrays.deepEquals(matrix2, other.matrix2);
     }
 
-    /**
-     * Calcule le vecteur entre deux points.
-     * @param p1 le premier point
-     * @param p2 le deuxième point
-     * @return le vecteur résultant du calcul
+        /**
+     * Vérifie si un point donné est à l'intérieur d'un polygone défini par une liste de points.
+     * 
+     * @param listePoint la liste des points définissant le polygone.
+     * @param p le point à vérifier.
+     * @return true si le point est à l'intérieur du polygone, false sinon.
      */
-    public static Point calculerVecteur(Point p1, Point p2) {
-        double deltaX = p2.getX() - p1.getX();
-        double deltaY = p2.getY() - p1.getY();
-        return new Point(deltaX, deltaY);
-    }
-
-    /**
-     * Calcule les indices pour chaque paire de points de contrôle.
-     * @param pointsDeControle les points de contrôle
-     * @param nbFrame le nombre de frames
-     * @return une liste de points représentant les indices
-     */
-    public List<Point> listIndice(PointDeControle pointsDeControle, int nbFrame) {
-        List<Point> p = new ArrayList<>();
-        for (Map.Entry<Point, Point> entry : pointsDeControle.getPointsMap().entrySet()) {
-            Point keyPoint = entry.getKey();
-            Point valuePoint = entry.getValue();
-            Point indice = calculerVecteur(keyPoint, valuePoint);
-            indice.setX(indice.getX() / nbFrame);
-            indice.setY(indice.getY() / nbFrame);
-            p.add(indice);
-        }
-        return p;
-    }
-
-    /**
-     * Applique le morphisme simple pour générer une animation GIF.
-     * @param image1 l'image source
-     * @param pointsDeControle les points de contrôle
-     * @param nbFrame le nombre de frames
-     * @throws IOException si une erreur d'entrée/sortie se produit
-     */
-    public void morphismeSimple(BufferedImage image1, PointDeControle pointsDeControle, int nbFrame) throws IOException {
-        Color[][] matrix = genererMatrice(image1);
-        List<Point> listIndice = listIndice(pointsDeControle, nbFrame); 
-        List<Point> pointsKeys = new ArrayList<>(pointsDeControle.getPointsMap().keySet());
-        Color couleur = (selectedColor != null) ? new Color((int)(selectedColor.getRed() * 255), (int)(selectedColor.getGreen() * 255), (int)(selectedColor.getBlue() * 255)) : chercheCouleur(matrix, pointsKeys);
-        Color autreCouleur = chercheAutreCouleur(matrix, pointsKeys);
+    public boolean estDomaine(List<Point> listePoint, Point p) {
+        int compteur = 0;
+        int nbPts = listePoint.size();
+        Point dernierPoint = listePoint.get(nbPts - 1);
     
-        ImageOutputStream output = new FileImageOutputStream(new File("animation.gif"));
-        GifSequenceWriter gifWriter = new GifSequenceWriter(output, image1.getType(), 100, true);
-    
-        int hauteur = matrix.length;
-        int largeur = matrix[0].length;
-    
-        for (int i = 0; i < nbFrame; i++) {
-            List<Point> listPoint = new ArrayList<>(); 
-            for (int j = 0; j < pointsKeys.size(); j++) {
-                Point p = pointsKeys.get(j);
-                Point indice = listIndice.get(j);
-                double x = Math.max(0, Math.min(largeur - 1, p.getX() + indice.getX() * i));
-                double y = Math.max(0, Math.min(hauteur - 1, p.getY() + indice.getY() * i));
-                Point p1 = new Point(x, y); 
-                listPoint.add(p1);
-            }
-            BufferedImage frameImage = morphismeSimpleRemplissage(matrix, couleur, autreCouleur, listPoint);
-            
-            gifWriter.writeToSequence(frameImage);
-        }
-    
-        // Ajouter l'image d'arrivée en utilisant les valeurs de la map en entrée
-        List<Point> listPointArrivee = new ArrayList<>();
-        for (Entry<Point, Point> entry : pointsDeControle.getPointsMap().entrySet()) {
-            listPointArrivee.add(entry.getValue());
-        }
-        BufferedImage imageArrivee = morphismeSimpleRemplissage(matrix, couleur, autreCouleur, listPointArrivee);
-        gifWriter.writeToSequence(imageArrivee);
-    
-        gifWriter.close();
-        output.close();
-    }
-    
-    
-    /**
-     * Remplit l'image en utilisant le morphisme simple.
-     * @param matrix la matrice de couleurs
-     * @param couleur la couleur à utiliser pour remplir l'intérieur du polygone
-     * @param autreCouleur la couleur à utiliser pour remplir l'extérieur du polygone
-     * @param points la liste des points
-     * @return l'image remplie
-     */
-    public BufferedImage morphismeSimpleRemplissage(Color[][] matrix, Color couleur, Color autreCouleur, List<Point> points) {
-        int hauteur = matrix.length;
-        int largeur = matrix[0].length;
-        for (int y = 0; y < hauteur; y++) {
-            for (int x = 0; x < largeur; x++) {
-                if (estDomaine(points, new Point(x, y))) {
-                    matrix[y][x] = couleur; // Correction : matrix[y][x] au lieu de matrix[x][y]
-                } else {
-                    matrix[y][x] = autreCouleur; // Correction : matrix[y][x] au lieu de matrix[x][y]
+        for (Point pointActuel : listePoint) {
+            // Vérifie si p est exactement sur un segment horizontal ou vertical
+            if (pointActuel.getY() == p.getY() && dernierPoint.getY() == p.getY()) {
+                // Segment horizontal
+                if ((pointActuel.getX() <= p.getX() && p.getX() <= dernierPoint.getX()) || 
+                    (dernierPoint.getX() <= p.getX() && p.getX() <= pointActuel.getX())) {
+                    return true; // Le point est sur un segment horizontal
+                }
+            } else if (pointActuel.getX() == p.getX() && dernierPoint.getX() == p.getX()) {
+                // Segment vertical
+                if ((pointActuel.getY() <= p.getY() && p.getY() <= dernierPoint.getY()) || 
+                    (dernierPoint.getY() <= p.getY() && p.getY() <= pointActuel.getY())) {
+                    return true; // Le point est sur un segment vertical
+                }
+            } else {
+                // Vérifie les intersections avec le segment actuel
+                if ((pointActuel.getY() < p.getY() && dernierPoint.getY() >= p.getY()) || 
+                    (pointActuel.getY() >= p.getY() && dernierPoint.getY() < p.getY())) {
+                    double intersectX = pointActuel.getX() + (p.getY() - pointActuel.getY()) * (dernierPoint.getX() - pointActuel.getX()) / (dernierPoint.getY() - pointActuel.getY());
+                    if (p.getX() < intersectX) {
+                        compteur++;
+                    }
                 }
             }
+            dernierPoint = pointActuel;
         }
-        return genereImage(matrix);
+        return compteur % 2 != 0;
     }
+   
+    
+
     
 }
