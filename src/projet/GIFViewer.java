3c832925a0f26176dde2d3dfb31d9ad109ad1616
Aure Nolhan <aurenolhan@cy-tech.fr>
2024-05-22 15:04:11 +0200

    rajout slider

--- src/projet/GIFViewer.java
+++ src/projet/GIFViewer.java
@@ -1,18 +1,37 @@
 package src.projet;
 
-import java.io.File;
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.embed.swing.SwingFXUtils;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class GIFViewer extends Application {
 
     private static String gifFilePath;
+    private List<Image> gifFrames;
+    private ImageView imageView;
+    private Slider slider;
+    private boolean isSliderMode = false;
 
     public static void display(String title, String filePath) throws IOException {
         gifFilePath = filePath;
@@ -31,14 +50,86 @@ public class GIFViewer extends Application {
         primaryStage.setAlwaysOnTop(true);
 
         File file = new File(gifFilePath);
-        Image gifImage = new Image(file.toURI().toString());
-        ImageView imageView = new ImageView(gifImage);
+        gifFrames = extractGifFrames(file);
+        if (gifFrames.isEmpty()) {
+            System.out.println("No frames extracted from the GIF.");
+            return;
+        }
 
-        StackPane root = new StackPane();
-        root.getChildren().add(imageView);
+        imageView = new ImageView(new Image(file.toURI().toString()));
 
-        Scene scene = new Scene(root, gifImage.getWidth(), gifImage.getHeight());
+        slider = new Slider(0, gifFrames.size() - 1, 0);
+        slider.setMajorTickUnit(1);
+        slider.setBlockIncrement(1);
+        slider.setSnapToTicks(true);
+        slider.setDisable(true);
+        slider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (!slider.isValueChanging() && isSliderMode) {
+                imageView.setImage(gifFrames.get(newVal.intValue()));
+            }
+        });
+        slider.setOnMousePressed(e -> {
+            if (isSliderMode) {
+                slider.setValueChanging(true);
+            }
+        });
+        slider.setOnMouseReleased(e -> {
+            if (isSliderMode) {
+                slider.setValueChanging(false);
+                imageView.setImage(gifFrames.get((int) slider.getValue()));
+            }
+        });
+
+        Button switchModeButton = new Button("Switch to Slider Mode");
+        switchModeButton.setOnAction(e -> {
+            isSliderMode = !isSliderMode;
+            if (isSliderMode) {
+                imageView.setImage(gifFrames.get((int) slider.getValue()));
+                switchModeButton.setText("Switch to Play Mode");
+                slider.setDisable(false);
+            } else {
+                imageView.setImage(new Image(file.toURI().toString()));
+                switchModeButton.setText("Switch to Slider Mode");
+                slider.setDisable(true);
+            }
+        });
+
+        HBox controls = new HBox(10, switchModeButton);
+        controls.setPadding(new Insets(10));
+        controls.setAlignment(Pos.CENTER);
+
+        StackPane imagePane = new StackPane(imageView);
+        BorderPane root = new BorderPane();
+        root.setCenter(imagePane);
+        root.setBottom(slider);
+        root.setTop(controls);
+        BorderPane.setMargin(slider, new Insets(10));
+        BorderPane.setMargin(controls, new Insets(10));
+
+        Scene scene = new Scene(root, gifFrames.get(0).getWidth(), gifFrames.get(0).getHeight() + 100);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private List<Image> extractGifFrames(File gifFile) throws IOException {
+        List<Image> frames = new ArrayList<>();
+        try (ImageInputStream stream = ImageIO.createImageInputStream(gifFile)) {
+            Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("gif");
+            if (readers.hasNext()) {
+                ImageReader reader = readers.next();
+                reader.setInput(stream);
+                int numFrames = reader.getNumImages(true);
+                for (int i = 0; i < numFrames; i++) {
+                    BufferedImage frame = reader.read(i);
+                    frames.add(SwingFXUtils.toFXImage(frame, null));
+                }
+                reader.dispose();
+            }
+        }
+        return frames;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
