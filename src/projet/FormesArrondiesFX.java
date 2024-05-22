03ce614cc157514de4a80d1108c1b90410fdea3a
gwennbrln <bgwenn33@gmail.com>
2024-05-22 10:02:34 +0200

    changement nolhan

--- src/projet/FormesArrondiesFX.java
+++ src/projet/FormesArrondiesFX.java
@@ -13,6 +13,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 import java.util.Map.Entry;
 
 public class FormesArrondiesFX extends FormesFX {
@@ -25,6 +26,26 @@ public class FormesArrondiesFX extends FormesFX {
     public FormesArrondiesFX(Canvas canvasA, Canvas canvasB, PointDeControle pointsDeControle) {
         super(canvasA, canvasB, pointsDeControle);
     }
+
+
+    public void loadExample() {
+        // Définir des images de départ et d'arrivée prédéfinies
+        Image startImage = new Image("file:img/carre.png");
+        Image endImage = new Image("file:img/triangle.png");
+        // Charger les images sur les canvas
+        canvasA.getGraphicsContext2D().drawImage(startImage, 0, 0);
+        canvasB.getGraphicsContext2D().drawImage(endImage, 0, 0);
+
+        // Définir des points de contrôle prédéfinis
+        pointsDeControle.getPointsMap().clear();
+        pointsDeControle.ajouter(new Point(100, 100), new Point(500, 100));
+        pointsDeControle.ajouter(new Point(200, 200), new Point(400, 200));
+        pointsDeControle.ajouter(new Point(300, 300), new Point(300, 300));
+        pointsDeControle.ajouter(new Point(400, 400), new Point(200, 400));
+
+        redrawPoints();
+    }
+
     private void checkForProximityAndMerge(double mouseX, double mouseY, boolean isImageA) {
         for (Entry<Point, Point> entry : pointsDeControle.getPointsMap().entrySet()) {
             Point point = isImageA ? entry.getKey() : entry.getValue();
@@ -35,7 +56,6 @@ public class FormesArrondiesFX extends FormesFX {
             }
         }
     }
-
     @Override
     public void handleMousePressed(MouseEvent mouseEvent, boolean isImageA) {
         double mouseX = mouseEvent.getX();
