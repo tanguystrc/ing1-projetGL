90632b1c5aa1be3f598481a4c47a47c672f1fa9a
gwennbrln <bgwenn33@gmail.com>
2024-05-24 11:52:49 +0200

    projet fin

--- src/projet/traitement/Visage.java
+++ src/projet/traitement/Visage.java
@@ -9,9 +9,12 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.BiConsumer;
 
 import javax.imageio.stream.FileImageOutputStream;
 import javax.imageio.stream.ImageOutputStream;
+
+import javafx.application.Platform;
 import src.projet.gif.GifSequenceWriter;
 
 /**
@@ -158,13 +161,13 @@ public class Visage {
      * @param nbFrame Nombre total de frames
      * @throws IOException
      */
-    public void morph() throws IOException {
+    public void morph(int dureeGIF,BiConsumer<Integer, Integer> progressUpdater) throws IOException {
         List<BufferedImage> morphFinal = new ArrayList<>();
         List<BufferedImage> morphs1 = new ArrayList<>();
         List<BufferedImage> morphs2 = new ArrayList<>();
 
         ImageOutputStream output = new FileImageOutputStream(new File("animation.gif"));
-        GifSequenceWriter gifWriter = new GifSequenceWriter(output, image1.getType(), 100, true);
+        GifSequenceWriter gifWriter = new GifSequenceWriter(output, image1.getType(), (dureeGIF*1000)/this.nbFrame, true);
 
         demiMorph(morphs1, morphs2, nbFrame);
 
@@ -180,10 +183,13 @@ public class Visage {
             }
             morphFinal.add(image);
             gifWriter.writeToSequence(image);
+            final int progress = k + 1;
+            Platform.runLater(() -> progressUpdater.accept(progress, this.nbFrame));
         }
 
         morphFinal.add(image2);
         gifWriter.writeToSequence(image2);
+        
 
         gifWriter.close();
         output.close();
