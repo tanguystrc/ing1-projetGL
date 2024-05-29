ca8c58a11621d87731364899159bd280adefc50c
Waoutch <ma3leve@gmail.com>
2024-05-29 09:04:33 +0200

    javadoc

--- src/projet/traitement/Point.java
+++ src/projet/traitement/Point.java
@@ -2,42 +2,74 @@ package src.projet.traitement;
 
 import java.util.Objects;
 
+/**
+ * Classe Point avec une abscisse et une ordonnée
+ */
 public class Point {
     private double x;
     private double y;
 
+    /**
+     * Constructeur de la classe Point
+     * @param x Abscisse
+     * @param y Ordonnée
+     */
     public Point(double x, double y) {
         setX(x);
         setY(y);
     }
 
+    /**
+     * Constructeur par default
+     */
     public Point() {
         this(0, 0);
     }
 
+    /**
+     * Getteur de l'abscisse
+     * @return double l'abscisse
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Setteur de l'abscisse
+     * @param x double
+     */
     public void setX(double x) {
      
             this.x = x;
 
     }
 
+    /**
+     * Getteur de l'ordonnée
+     * @return double l'ordonnée
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Setteur de l'ordonnée
+     * @param y double
+     */
     public void setY(double y) {
      
             this.y = y;
         
     }
 
-    public double distance(Point other) {
-        double dx = this.x - other.x;
-        double dy = this.y - other.y;
+    /**
+     * Retourne la distance entre deux point
+     * @param p Point
+     * @return La distance sous forme d'un double
+     */
+    public double distance(Point p) {
+        double dx = this.x - p.x;
+        double dy = this.y - p.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 
@@ -53,11 +85,19 @@ public class Point {
         return new Point(deltaX, deltaY);
     }
 
+    /**
+     * Fonction ToString pour les points
+     * @return (x,y)
+     */
     @Override
     public String toString() {
         return "Point : (" + x + "," + y + ")";
     }
 
+    /**
+     * Fonction equals pour les points
+     * @return True si l'argument et identique au point
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -66,31 +106,53 @@ public class Point {
         if (!(obj instanceof Point)) {
             return false;
         }
-        Point other = (Point) obj;
+        Point p = (Point) obj;
         final double EPSILON = 1e-10;
-        return Math.abs(this.x - other.x) < EPSILON && Math.abs(this.y - other.y) < EPSILON;
+        return Math.abs(this.x - p.x) < EPSILON && Math.abs(this.y - p.y) < EPSILON;
     }
 
+    /**
+     * Fonction de hashage implémenté aux point
+     */
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
     }
 
+    /**
+     * Renvoie la somme des deux points
+     * @param p Point
+     * @return La somme des point
+     */
     public Point somme(Point p) {
 		return new Point(x+p.getX(),y+p.getY());
 	}
 	
+    /**
+     * Renvoie la diffèrence entre le point et le point argument
+     * @param p Point
+     * @return La soustraction entre les deux points
+     */
 	public Point soustraction(Point p) {
 		return new Point(x-p.getX(),y-p.getY());
 	}
 	
+    /**
+     * Renvoie le vecteur perpendiculaire 
+     * @return Point symbolisant le vecteur perpendiculaire
+     */
 	public Point perp() {
 		return new Point(y,-x);
 	}
 	
+    /**
+     * Renvoie la norme du vecteur
+     * @return La norme
+     */
 	public double norme() {
 		return Math.sqrt(x*x+y*y);
 	}
+
 	/**
      * Produit scalaire en prenant les points pour des vecteurs
      * @param p
@@ -100,10 +162,19 @@ public class Point {
 		return x*p.getX()+y*p.getY();
 	}
 	
+    /**
+     * Renvoie le produit du point avec un scalaire
+     * @param v Double
+     * @return Le produit
+     */
 	public Point produit(double v) {
 		return new Point(x*v,y*v);
 	}
 
+    /**
+     * Renvoie le pixel le plus proche du point
+     * @return Un point en valeur entière
+     */
     public Point pixel(){
         return new Point(Math.round(x),Math.round(y));
     }
--- src/projet/traitement/Segment.java
+++ src/projet/traitement/Segment.java
@@ -1,21 +1,36 @@
 package src.projet.traitement;
 /**
- * Class représentant les segments pour le morphisme de visage
+ * Class représentant les segments pour le morphing de visage
  */
 public class Segment {
 	private Point debut;
 	private Point fin;
 	
+	/**
+	 * Constructeur de la classe Segment
+	 * @param debut Premier point
+	 * @param fin Second point
+	 */
 	public Segment(Point debut, Point fin) {
 		this.debut = debut;
 		this.fin = fin;
 	}
 	
+	/**
+	 * Constructeur de la classe Segment par défault
+	 */
 	public Segment() {
 		this.debut = new Point();
 		this.fin = new Point();
 	}
 	
+	/**
+	 * Constructeur de la classe Segment en rentrant les coordonnées des extrémités
+	 * @param x1 Abscisse du premier point
+	 * @param y1 Ordonnée du premier point
+	 * @param x2 Abscisse du second point
+	 * @param y2 Abscisse du second point
+	 */
 	public Segment(double x1,double y1,double x2,double y2) {
 		Point d = new Point (x1,y1);
 		Point f = new Point(x2,y2);
@@ -23,24 +38,52 @@ public class Segment {
 		this.fin=f;
 	}
 	
+	/**
+	 * Getteur du premier point
+	 * @return Le premier point
+	 */
 	public Point getDebut() {
 		return debut;
 	}
+
+	/**
+	 * Setteur du premier point
+	 * @param debut Point
+	 */
 	public void setDebut(Point debut) {
 		this.debut = debut;
 	}
+
+	/**
+	 * Getteur du second Point
+	 * @return Le second point
+	 */
 	public Point getFin() {
 		return fin;
 	}
+
+	/**
+	 * Setteur du second point
+	 * @param fin Point
+	 */
 	public void setFin(Point fin) {
 		this.fin = fin;
 	}
 	
+	/**
+	 * Transformer un segment en String
+	 * @return Le Segment sous forme ((x1,y1),(x2,y2))
+	 */
 	@Override
 	public String toString() {
 		return "("+debut.toString()+fin.toString()+")";
 	}
 	
+	/**
+	 * Fonction equals pour les Segments
+	 * @param o Object
+	 * @return True si le segment est identique à celui en paramètre
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof Segment) {
